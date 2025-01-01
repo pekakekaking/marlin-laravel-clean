bf4e934245dbdd7b39db3fd1b900c6a182c7431d
user <johnysinner89@gmail.com>
2025-01-01 23:03:07 +0700

    crud categories

---
+++ app/Http/Resources/PostResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PostResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'content' => $this->content,
+            'user_id' => $this->user_id,
+            'category_id' => $this->category_id,
+            'look' => $this->look,
+            'is_published' => $this->is_published,
+            'category'=>$this->category
+        ];
+    }
+}
